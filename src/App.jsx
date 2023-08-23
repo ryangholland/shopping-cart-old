e9c86748d50ca132e67944caa86c13369b5a87c1
ryangholland <rghcode@gmail.com>
2023-08-23 19:35:23 -0400

    Fix layout width problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
 function App() {
   return (
-    <main className="md:w-[760px] lg:w-[1000px] xl:w-[1280px] p-6">
+    <main className="xl:max-w-[1280px] p-6 m-auto">
       <nav className="flex items-center justify-between px-6 py-4 gap-12 flex-col md:flex-row">
         <h1 className="text-4xl font-bold">ShopMart</h1>
         <ul className="flex gap-8 text-md flex-col sm:flex-row">
-          <li><a href="#" className="underline-offset-4 hover:underline">All</a></li>
-          <li><a href="#" className="underline-offset-4 hover:underline">Women's</a></li>
-          <li><a href="#" className="underline-offset-4 hover:underline">Men's</a></li>
-          <li><a href="#" className="underline-offset-4 hover:underline">Jewelry</a></li>
-          <li><a href="#" className="underline-offset-4 hover:underline">Electronics</a></li>
+          <li>
+            <a href="#" className="underline-offset-4 hover:underline">
+              All
+            </a>
+          </li>
+          <li>
+            <a href="#" className="underline-offset-4 hover:underline">
+              Women's
+            </a>
+          </li>
+          <li>
+            <a href="#" className="underline-offset-4 hover:underline">
+              Men's
+            </a>
+          </li>
+          <li>
+            <a href="#" className="underline-offset-4 hover:underline">
+              Jewelry
+            </a>
+          </li>
+          <li>
+            <a href="#" className="underline-offset-4 hover:underline">
+              Electronics
+            </a>
+          </li>
         </ul>
-        <a href="#"><img src="/cart.svg" alt="Cart" className="border-2 border-white max-w-[50px] hover:border-black" /></a>
+        <a href="#">
+          <img
+            src="/cart.svg"
+            alt="Cart"
+            className="border-2 border-white max-w-[50px] hover:border-black"
+          />
+        </a>
       </nav>
       <hr className="border-black" />
       <section className="my-12 flex justify-center gap-16 items-center mx-4">
